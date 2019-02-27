13290e04f2b5f4c4796dfbf2f80daf774779998f
Sampson Liao <sampsonliao@Sampsons-Mac.local>
2019-02-26 23:10:30 -0800

    image feed

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -58,12 +58,14 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let image = info[.editedImage] as! UIImage
-        let size = CGSize(width: 300, height: 300)
-        let scaledImage = image.af_imageScaled(to: size)
         
-        imageView.image = scaledImage
+        if let image = info[.editedImage] as? UIImage{
+            let size = CGSize(width: 300, height: 300)
+            let scaledImage = image.af_imageScaled(to: size)
+            
+            imageView.image = scaledImage
         
+        }
         dismiss(animated: true, completion: nil)
     }
     /*
