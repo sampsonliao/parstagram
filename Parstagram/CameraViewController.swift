30243905e056a0ebb46afe8b3862a3894dc17ab0
Sampson Liao <sampsonliao@Sampsons-Mac.local>
2019-02-26 21:45:56 -0800

    fixed constraint issue

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -18,7 +18,7 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Do any additional setup ..after loading the view.
     }
     
 
